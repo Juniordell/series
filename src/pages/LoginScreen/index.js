e549a4f0ab04f9a18288ce14f4ade5b473a01663
Nelson Jr <juniordell123@gmail.com>
2020-08-12 19:52:33 -0300

    Add Login Screen and the style

---
+++ src/pages/LoginScreen/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+import FormRow from '../../components/FormRow';
+
+const LoginScreen = () => {
+  return (
+      <Container>
+          <FormRow>
+              
+          </FormRow>
+      </Container>
+  );
+}
+
+export default LoginScreen;
