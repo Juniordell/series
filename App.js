88c6dc11124e39e26edcb19f2c491439f7d36d39
Nelson Jr <juniordell123@gmail.com>
2020-08-12 22:45:58 -0300

    Fix bugs and add Firebase

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import LoginScreen from './src/pages/LoginScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  'Login': {
+    screen: LoginScreen
   },
-});
+},
+  {
+    defaultNavigationOptions: {
+      title: 'Series!',
+
+      headerTintColor: 'white',
+      
+      headerStyle: {
+        backgroundColor: '#6ca2f7',
+        borderBottomWidth: 1,
+        borderBottomColor: '#C5C5C5'
+      },
+
+      headerTitleStyles: {
+        color: 'white'
+      }
+    }
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default AppContainer
