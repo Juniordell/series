8e91ed2b646ddcaec57eba6d659df8dea541c338
Nelson Jr <juniordell123@gmail.com>
2020-08-12 22:44:52 -0300

    Updating the Login Screen

--- src/pages/LoginScreen/index.js
+++ src/pages/LoginScreen/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Input, ButtonSubmit } from './styles';
 import FormRow from '../../components/FormRow';
 
 const LoginScreen = () => {
+
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  useEffect(() => {
+    var firebaseConfig = {
+      apiKey: "AIzaSyCDNZlHwc592UmX5V34NjVf7AbtpQwohJg",
+      authDomain: "series-2ba1a.firebaseapp.com",
+      databaseURL: "https://series-2ba1a.firebaseio.com",
+      projectId: "series-2ba1a",
+      storageBucket: "series-2ba1a.appspot.com",
+      messagingSenderId: "157281159962",
+      appId: "1:157281159962:web:67c85b69b49062a24a7aa3",
+      measurementId: "G-Z7RW0Z37HC"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }, [])
+  
+  function tryLogin() {
+
+  }
+
   return (
       <Container>
-          <FormRow>
-              
+
+          <FormRow first>
+              <Input  
+                placeholder='user@email.com'
+                value={email}
+                onChange={text => setEmail(text)}
+              />
           </FormRow>
+
+          <FormRow last>
+              <Input  
+                placeholder='*******'
+                secureTextEntry
+                value={password}
+                onChange={text => setPassword(text)}
+              />
+          </FormRow>
+
+          <ButtonSubmit
+            onPress={tryLogin}
+            title='Entrar'
+          />
+
       </Container>
   );
 }
--- src/pages/LoginScreen/styles.js
+++ src/pages/LoginScreen/styles.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  
+  padding: 0 10px;
 `;
+
+export const Input = styled.TextInput`
+    padding: 0 5px 5px;
+` 
+
+export const ButtonSubmit = styled.Button`
+    margin-top: 5px;
+` 
